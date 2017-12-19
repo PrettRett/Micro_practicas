33682c300abeefc98b32797476e8d7aa1da1e74f
dani <danistons@hotmail.com>
2017-12-19 10:53:51 +0100

    Starting point

---
+++ Debug/Main_Sumo.d
@@ -0,0 +1,73 @@
+# FIXED
+
+Main_Sumo.obj: ../Main_Sumo.c
+Main_Sumo.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdbool.h
+Main_Sumo.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h
+Main_Sumo.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/time.h
+Main_Sumo.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/linkage.h
+Main_Sumo.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/abi_prefix.h
+Main_Sumo.obj: ../inc/hw_memmap.h
+Main_Sumo.obj: ../inc/hw_types.h
+Main_Sumo.obj: ../inc/hw_ints.h
+Main_Sumo.obj: ../driverlib/gpio.h
+Main_Sumo.obj: ../driverlib/pin_map.h
+Main_Sumo.obj: ../driverlib/rom.h
+Main_Sumo.obj: ../driverlib/sysctl.h
+Main_Sumo.obj: ../driverlib/uart.h
+Main_Sumo.obj: ../driverlib/interrupt.h
+Main_Sumo.obj: ../driverlib/adc.h
+Main_Sumo.obj: ../driverlib/timer.h
+Main_Sumo.obj: ../utils/uartstdio.h
+Main_Sumo.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdarg.h
+Main_Sumo.obj: ../drivers/buttons.h
+Main_Sumo.obj: ../drivers/rgb.h
+Main_Sumo.obj: C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/FreeRTOS.h
+Main_Sumo.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stddef.h
+Main_Sumo.obj: C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOSConfig.h
+Main_Sumo.obj: C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/utils/RunTimeStatsConfig.h
+Main_Sumo.obj: C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/projdefs.h
+Main_Sumo.obj: C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/portable.h
+Main_Sumo.obj: C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/deprecated_definitions.h
+Main_Sumo.obj: C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/portable/CCS/ARM_CM4F/portmacro.h
+Main_Sumo.obj: C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/mpu_wrappers.h
+Main_Sumo.obj: C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/task.h
+Main_Sumo.obj: C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/list.h
+Main_Sumo.obj: C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/queue.h
+Main_Sumo.obj: C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/semphr.h
+Main_Sumo.obj: ../utils/cpu_usage.h
+
+../Main_Sumo.c: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdbool.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/time.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/linkage.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/abi_prefix.h: 
+../inc/hw_memmap.h: 
+../inc/hw_types.h: 
+../inc/hw_ints.h: 
+../driverlib/gpio.h: 
+../driverlib/pin_map.h: 
+../driverlib/rom.h: 
+../driverlib/sysctl.h: 
+../driverlib/uart.h: 
+../driverlib/interrupt.h: 
+../driverlib/adc.h: 
+../driverlib/timer.h: 
+../utils/uartstdio.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdarg.h: 
+../drivers/buttons.h: 
+../drivers/rgb.h: 
+C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/FreeRTOS.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stddef.h: 
+C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOSConfig.h: 
+C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/utils/RunTimeStatsConfig.h: 
+C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/projdefs.h: 
+C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/portable.h: 
+C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/deprecated_definitions.h: 
+C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/portable/CCS/ARM_CM4F/portmacro.h: 
+C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/mpu_wrappers.h: 
+C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/task.h: 
+C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/list.h: 
+C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/queue.h: 
+C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/semphr.h: 
+../utils/cpu_usage.h: 
---
+++ Debug/commands.d
@@ -0,0 +1,73 @@
+# FIXED
+
+commands.obj: ../commands.c
+commands.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdbool.h
+commands.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h
+commands.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdio.h
+commands.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/linkage.h
+commands.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdarg.h
+commands.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/string.h
+commands.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdlib.h
+commands.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/assert.h
+commands.obj: C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/FreeRTOS.h
+commands.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stddef.h
+commands.obj: C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOSConfig.h
+commands.obj: C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/utils/RunTimeStatsConfig.h
+commands.obj: C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/projdefs.h
+commands.obj: C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/portable.h
+commands.obj: C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/deprecated_definitions.h
+commands.obj: C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/portable/CCS/ARM_CM4F/portmacro.h
+commands.obj: C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/mpu_wrappers.h
+commands.obj: C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/task.h
+commands.obj: C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/list.h
+commands.obj: C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/queue.h
+commands.obj: C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/semphr.h
+commands.obj: ../inc/hw_memmap.h
+commands.obj: ../inc/hw_sysctl.h
+commands.obj: ../inc/hw_types.h
+commands.obj: ../driverlib/debug.h
+commands.obj: ../driverlib/gpio.h
+commands.obj: ../driverlib/uart.h
+commands.obj: ../driverlib/sysctl.h
+commands.obj: ../driverlib/rom.h
+commands.obj: ../utils/cpu_usage.h
+commands.obj: ../utils/cmdline.h
+commands.obj: ../utils/uartstdio.h
+commands.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdarg.h
+commands.obj: ../drivers/rgb.h
+
+../commands.c: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdbool.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdio.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/linkage.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdarg.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/string.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdlib.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/assert.h: 
+C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/FreeRTOS.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stddef.h: 
+C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOSConfig.h: 
+C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/utils/RunTimeStatsConfig.h: 
+C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/projdefs.h: 
+C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/portable.h: 
+C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/deprecated_definitions.h: 
+C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/portable/CCS/ARM_CM4F/portmacro.h: 
+C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/mpu_wrappers.h: 
+C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/task.h: 
+C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/list.h: 
+C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/queue.h: 
+C:/Users/Alumno/EspacioTrabajo/Programa_Sumo/FreeRTOS/Source/include/semphr.h: 
+../inc/hw_memmap.h: 
+../inc/hw_sysctl.h: 
+../inc/hw_types.h: 
+../driverlib/debug.h: 
+../driverlib/gpio.h: 
+../driverlib/uart.h: 
+../driverlib/sysctl.h: 
+../driverlib/rom.h: 
+../utils/cpu_usage.h: 
+../utils/cmdline.h: 
+../utils/uartstdio.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdarg.h: 
+../drivers/rgb.h: 

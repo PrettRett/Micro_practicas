5def91c7151551d416ffaec11353e31c10616a17
Daniel <Daniel@DESKTOP-FUNHEA2>
2018-01-04 23:28:51 +0100

    Añadidas las funciones de preparar motores y para preparar los enconders la función de los enconders no está completa

--- PID.c
+++ PID.c
@@ -4,8 +4,28 @@
  *  Created on: 21 dic. 2017
  *      Author: Alumno
  */
+#include "PID.h"
 
 void PIDTask(void *pvParameters)
+{
+
+}
+
+void Prep_Motores ()
+{
+    SysCtlPeripheralSleepEnable(SYSCTL_PERIPH_PWM1);
+    GPIOPinConfigure(GPIO_PF2_M1PWM6);
+    GPIOPinConfigure(GPIO_PF3_M1PWM7);
+    GPIOPinTypePWM(GPIO_PORTF_BASE, GPIO_PIN_2);
+    GPIOPinTypePWM(GPIO_PORTF_BASE, GPIO_PIN_3);
+    PWMGenConfigure(PWM1_BASE, PWM_GEN_3, PWM_GEN_MODE_DOWN |
+                     PWM_GEN_MODE_NO_SYNC);
+    PWMGenPeriodSet(PWM1_BASE, PWM_GEN_3, PERIOD_PWM);
+    PWMPulseWidthSet(PWM1_BASE, PWM_OUT_6,STOPCOUNT);
+    PWMPulseWidthSet(PWM1_BASE, PWM_OUT_7,STOPCOUNT);
+}
+
+void Prep_Encoders()
 {
 
 }
--- PID.h
+++ PID.h
@@ -8,8 +8,31 @@
 #ifndef PID_H_
 #define PID_H_
 
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_types.h"
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/timer.h"
+#include "driverlib/pin_map.h" // Include para poder configurar el pin como salida PWM#include <stdbool.h>
+#include "driverlib/pwm.h"
+#include "driverlib/sysctl.h"
+#include "inc/hw_nvic.h"
+
+#define PERIOD_PWM SysCtlClockGet()/64*0.02
+#define STOPCOUNT (PERIOD_PWM)/20*1.528
+#define COUNT_1MS PERIOD_PWM/20
+#define COUNT_2MS PERIOD_PWM/10
+#define NUM_STEPS 50
+#define CYCLE_INCREMENTS (abs(COUNT_1MS-COUNT_2MS))/NUM_STEPS
 
 void PIDTask (void *pvParameters);
+
+void Prep_Motores();
+
+void Prep_Encoders();
 
 
 #endif /* PID_H_ */
